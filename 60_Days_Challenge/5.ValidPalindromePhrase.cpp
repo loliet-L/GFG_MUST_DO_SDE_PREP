02535baeda807142b0b4d6cf50c909e6ef0c2ed7
Vivek Lone <79907341+loliet-L@users.noreply.github.com>
2023-08-26 22:46:28 +0530

    POTD

---
+++ 60_Days_Challenge/5.ValidPalindromePhrase.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase 
+// letters and removing all non-alphanumeric characters, it reads the same forward and backward. 
+// Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+
+
+
+bool isPalindrome(string s) {
+for (int i = 0, j = s.size() - 1; i < j; i++, j--) { // Move 2 pointers from each end until they collide
+        while (isalnum(s[i]) == false && i < j) i++; // Increment left pointer if not alphanumeric
+        while (isalnum(s[j]) == false && i < j) j--; // Decrement right pointer if no alphanumeric
+        if (toupper(s[i]) != toupper(s[j])) return false; // Exit and return error if not match
+    }
+    
+    return true;
+
+        
+    } 
